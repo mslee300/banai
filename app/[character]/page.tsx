aebb9f810a7dc1aa18d6741bbef046c85b37a6b8
Minseok Lee <mslee300@bu.edu>
2025-04-03 18:10:19 -0400

    fix Vercel error7

--- app/[character]/page.tsx
+++ app/[character]/page.tsx
@@ -1,8 +1,7 @@
-// app/[character]/page.tsx
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import ChatClient from "./ChatClient";
 
-export default async function ChatPage(props: any) {
-  const { params } = props;
+export default async function ChatPage({ params }: any) {
   const resolvedParams = await Promise.resolve(params);
   return <ChatClient characterId={resolvedParams.character} />;
 }
